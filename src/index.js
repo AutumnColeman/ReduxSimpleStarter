09476a61cc94e44912268ca77ea86e48de53886c
Autumn Coleman <autumn.eve.coleman@gmail.com>
2017-02-16 22:21:27 -0500

    Update readme

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyABqs4_2twut4EQXmSziStKGZay1rlv8g4';
+
 // Create a new component.  This component should produce some HTML.
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put in on the page (in the DOM)
